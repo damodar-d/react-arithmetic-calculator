f8ad5e4dcd40f362c430da407fb282b2c8813c5f
Abhiram@Jumbo <designing.developer111@gmail.com>
2023-11-24 13:09:41 +0530

    Improve: Added Navbar for consistent look

---
+++ src/Components/Navbar.js
@@ -0,0 +1,13 @@
+import '../Styles/Navbar.css'
+
+function Navbar(props){
+    return(
+        <div>
+            <nav>
+                <h1>Simplest Working React Calculator</h1>
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar;
